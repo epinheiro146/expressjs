9dfd70d3944ac971711bc0cbdb3fa43e755996b1
epinh <epinheiro146@gmail.com>
2023-02-27 23:57:13 -0600

    completing lab

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 let app = express();
+
+let submissions = [];
 
 app.use((req, res, next) => {
     console.log(req.originalUrl);
@@ -12,9 +15,25 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/game-survey', (req, res) => {
-    console.log(req.body.name);
-    console.log(req.body.fav_game);
+    //console.log(req.body.name);
+    //console.log(req.body.fav_game);
+
+    submissions.push({
+        "name": `${req.body.name}`,
+        "fav_game": `${req.body.fav_game}`
+    });
+
+    fs.writeFile(path.join(__dirname, '../form-submissions.json'), JSON.stringify(submissions), err => {
+        if (err) console.log(err);
+    });
+
     res.send('Thanks for submitting!');
+});
+
+app.get('/formsubmissions', (req, res) => {
+    const rawdata = fs.readFileSync(path.join(__dirname, '../form-submissions.json'));
+    const data = JSON.parse(rawdata);
+    res.json(data);
 });
 
 // app.get('/', (req, res) => {
